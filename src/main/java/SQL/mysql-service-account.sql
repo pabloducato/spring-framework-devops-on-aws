7aee9a6ed0a89d55536ff3afb1c74b4e0dad0572
Paweł Kocan <pkocan@onet.pl>
2023-07-23 20:26:05 +0200

    [KOCAN-0001] mysql-service-account.sql

---
+++ src/main/java/SQL/mysql-service-account.sql
@@ -0,0 +1,6 @@
+create user 'springframework'@'localhost' identified by 'guru';
+
+grant select ON springguru.* to 'springframework'@'localhost';
+grant insert ON springguru.* to 'springframework'@'localhost';
+grant delete ON springguru.* to 'springframework'@'localhost';
+grant update ON springguru.* to 'springframework'@'localhost';
