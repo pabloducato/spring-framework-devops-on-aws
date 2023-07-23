8b5332033e1ad55718e33ccb588ea3c2fe72d317
Paweł Kocan <pkocan@onet.pl>
2023-07-23 19:59:55 +0200

    [KOCAN-0001] Try-catch and YAML

--- src/main/java/pl/kocan/springframeworkdevopsonaws/SpringFrameworkDevopsOnAwsApplication.java
+++ src/main/java/pl/kocan/springframeworkdevopsonaws/SpringFrameworkDevopsOnAwsApplication.java
@@ -1,12 +1,18 @@
 package pl.kocan.springframeworkdevopsonaws;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringFrameworkDevopsOnAwsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFrameworkDevopsOnAwsApplication.class, args);
+		try {
+			SpringApplication.run(SpringFrameworkDevopsOnAwsApplication.class, args);
+		} catch (RuntimeException ex) {
+			log.info("An unexpected error has occured");
+		}
 	}
 }
